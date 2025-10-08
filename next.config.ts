b1d0331bc737a63edc28a0d1286c47188116e08a
Santiago Gericke <santiago.gericke@techtorch.io>
2025-10-07 18:16:02 -0700

    relative path fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: path.join(__dirname, '.'),
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '.'),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
